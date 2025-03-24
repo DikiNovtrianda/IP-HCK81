c036fb9765242a993ee9c5b61dbbac4ecf8c6a55
dikinovtrianda <dikinovtrianda@gmail.com>
2025-03-24 20:33:28 +0700

    feat : adding jwt

---
+++ server/migrations/20250324132612-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      preferedCategory: {
+        type: Sequelize.STRING
+      },
+      hatedCategory: {
+        type: Sequelize.STRING
+      },
+      platform: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
