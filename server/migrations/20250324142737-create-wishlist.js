f18a404fa1f71cc6b4302e7433aa9cf46ff13b47
dikinovtrianda <dikinovtrianda@gmail.com>
2025-03-24 21:45:18 +0700

    feat : add wishlist model and migration

---
+++ server/migrations/20250324142737-create-wishlist.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Wishlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      gameId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      status: {
+        defaultValue: 'Wishlist',
+        type: Sequelize.STRING
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Wishlists');
+  }
+};
