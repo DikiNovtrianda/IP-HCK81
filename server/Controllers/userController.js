4801b9bb14413f5982bfa74625043b790c7fbb98
dikinovtrianda <dikinovtrianda@gmail.com>
2025-03-24 23:52:32 +0700

    feat : register route added

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,3 +1,22 @@
+const { User, Wishlist } = require('../models');
+const { comparePass } = require('../helpers/bcrypt');
+const { signToken } = require('../helpers/jwt');
+
 module.exports = class userController {
-    
+    static async register(req, res, next) {
+        try {
+            const { username, email, password } = req.body;
+            let status = await User.create({
+                username,
+                email,
+                password
+            })
+            console.log('>>>',status);
+            
+            res.status(201).json({ username: status.username, email: status.email });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'DEI' });
+        }
+    }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { home } = require("../Controllers/gameController");
+const { register, login } = require("../Controllers/userController");
 const router = express.Router();
 
 router.get('/', home)
+router.post('/register', register)
 
 module.exports = router;
