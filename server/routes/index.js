27d33df43d648d54b10f4e20e41527a24f6e8766
dikinovtrianda <dikinovtrianda@gmail.com>
2025-03-25 17:24:47 +0700

    feat : client side with vite added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const { getGames, getGamesJson } = require("../Controllers/gameController");
+const { getGames } = require("../Controllers/gameController");
 const { register, login } = require("../Controllers/userController");
 const authentication = require("../middlewares/authentication");
-const { createWishlist } = require("../Controllers/wishlistController");
+const { createWishlist, getWishlist, deleteWishlist, boughtWishlist, getComment, addComment } = require("../Controllers/wishlistController");
 const router = express.Router();
 
-// router.get('/')
 router.post('/register', register)
 router.post('/login', login)
-// router.get('/get-games', getGamesJson)
 
 router.use(authentication)
+router.get('/wishlist/:gameId', getWishlist)
 router.post('/wishlist/:gameId', createWishlist)
+router.delete('/wishlist/:gameId', deleteWishlist)
+router.patch('/wishlist/:gameId', boughtWishlist)
+router.get('/wishlist/:gameId/comment', getComment)
+router.post('/wishlist/:gameId/comment', addComment)
 
 module.exports = router;
