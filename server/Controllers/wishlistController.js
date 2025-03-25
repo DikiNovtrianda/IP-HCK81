6e3a25745665c0fcbbb448bf19ec595160331dee
dikinovtrianda <dikinovtrianda@gmail.com>
2025-03-25 16:59:23 +0700

    feat : bought wishlist

--- server/Controllers/wishlistController.js
+++ server/Controllers/wishlistController.js
@@ -25,4 +25,30 @@ module.exports = class wishlistController {
             next(error);
         }
     }
+
+    static async boughtWishlist(req, res, next) {
+        try {
+            const { userId } = req.body
+            const { gameId } = req.params
+            let findGame = await Game.findOne({ where: { id: gameId } })
+            if (!findGame) {
+                throw {
+                    name: "NotFound",
+                    message: "Game not found!"
+                }
+            }
+            let findComment = await Wishlist.findOne({ where: { userId, gameId } })
+            if (findComment.status === 'Bought') {
+                throw {
+                    name: "BadRequest",
+                    message: "Game already bought!"
+                }
+            }
+
+            await Wishlist.update({ status: 'Bought' }, { where: { userId, gameId } })
+            res.status(200).json({ message: "Game library added!" });
+        } catch (error) {
+            next(error);
+        }
+    }
 }
