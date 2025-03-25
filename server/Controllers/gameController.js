72399a8cfdd25d29ff442830e9d94edf5fbf0334
dikinovtrianda <dikinovtrianda@gmail.com>
2025-03-25 17:38:11 +0700

    feat : get detailed game

--- server/Controllers/gameController.js
+++ server/Controllers/gameController.js
@@ -9,4 +9,14 @@ module.exports = class gameController {
             next(error)
         }
     }
+
+    static async getDetailedGames(req, res, next) {
+        try {
+            const { gameId } = req.params
+            const game = await Game.findByPk(gameId)
+            res.status(200).json(game)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
